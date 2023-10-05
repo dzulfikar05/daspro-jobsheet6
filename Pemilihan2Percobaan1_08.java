a39ad464254e0bd4dc0e015eddf4f83a8ba841e6
dzulfikarmagang123 <fikaralghifari0504@gmail.com>
2023-10-05 07:45:44 +0700

    adjustment 1 percobaan 1

--- Pemilihan2Percobaan1_08.java
+++ Pemilihan2Percobaan1_08.java
@@ -12,6 +12,8 @@ public class Pemilihan2Percobaan1_08 {
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0) {
                 System.out.println("Tahun tahun Kabisat");
+            } else {
+                System.out.println("Bukan tahun Kabisat");
             }
         } else {
             System.out.println("Bukan tahun Kabisat");
